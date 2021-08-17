d971157e8556945cc6d44a079097fd9532f8c555
landryonana <51939280+landryonana@users.noreply.github.com>
2021-08-17 08:15:11 +0100

    Update urls.py

--- rapport/urls.py
+++ rapport/urls.py
@@ -10,6 +10,4 @@ app_name = 'rapport'
 urlpatterns = [
     path('', views.index_rapport, name="index_rapport"),
     path('rapport-detail/<str:mois>/', views.rapport_evang_detail_sortie, name="rapport_evang_detail_sortie"),
-    path('rapport-pdf', views.rapport_pdf, name="rapport_pdf"),
-    path('rapport-excel', views.rapport_excel, name="rapport_excel"),
 ]
